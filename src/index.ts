504509fa9666e34c5186f1780ea88ae17345d4be
Alexander Darovsky <adarovsky@me.com>
2019-10-10 17:27:38 +0500

    export image containers and resize observers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+export {resizeObserver} from "./resize_sensor";
 export {Engine} from "./engine";
 export {Layout} from "./layout";
 export {ReactViewReference as ReactView, ReactViewProps, ReactViewState} from "./react_views";
-export {ColorContainer as Color} from './types';
+export {ColorContainer as Color, ImageContainer as Image} from './types';
+
 import './vlayout.css';
