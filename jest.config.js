0f537d7f20b08b330079427c5dc5d33e4cc5abaa
Alexander Darovsky <adarovsky@me.com>
2019-10-14 22:44:25 +0500

    ignore generated dirs from stat report

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "transform": {
         "^.+\\.tsx?$": "ts-jest"
     },
+    "coveragePathIgnorePatterns": ["/node_modules/", "/lib/", "/example/", "/doc/", "/__mocks/"],
     "setupFilesAfterEnv": ["<rootDir>src/setupEnzyme.ts"],
     "moduleNameMapper": {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
