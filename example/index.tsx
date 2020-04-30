aae43afafcff1bb3234d08fdb24369f51eba3a5b
Alexander Darovsky <adarovsky@me.com>
2020-04-30 23:57:11 +0500

    style inject fix

--- example/index.tsx
+++ example/index.tsx
@@ -3,12 +3,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
-const Example = () => {
-  return (
-    <div>
-      <App />
-    </div>
-  );
-};
-
-ReactDOM.render(<Example />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
--- tsdx.config.js
+++ tsdx.config.js
@@ -1,9 +1,10 @@
 const postcss = require('rollup-plugin-postcss');
+// const css = require ('rollup-plugin-css-only');
+
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
       postcss({
-        modules: true,
       })
     );
     return config;
