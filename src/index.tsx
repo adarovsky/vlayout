0614b8f75cc96852075c4a3f181d528ab21e1e82
Alexander Darovsky <adarovsky@me.com>
2020-07-17 21:32:58 +0500

    enable build for isolated modules

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ export {Engine} from "./engine";
 export {Layout} from "./layout";
 import './vlayout.css';
 
-export {ReactViewReference as ReactView, ReactViewProps, ReactViewState} from "./react_views";
+export { ReactViewReference as ReactView } from './react_views';
+export type { ReactViewProps, ReactViewState } from "./react_views";
 export {View} from "./view";
-export {ColorContainer as Color, ImageContainer as Image, SimpleListDefinitionItem, ListDefinitionItem, ComplexDefinitionItem} from './types';
-export { ListModelItem } from './list';
+export { ColorContainer as Color, ImageContainer as Image } from './types';
+export type { SimpleListDefinitionItem, ListDefinitionItem, ComplexDefinitionItem } from './types';
+export type { ListModelItem } from './list';
 export { LayoutComponent } from './layout_component'
 export { connect } from './connect'
