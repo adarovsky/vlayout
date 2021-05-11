1fe763b885c19e0031ab45c6d6dc196f8a4c44ad
Alexander Darovsky <adarovsky@me.com>
2021-05-11 11:00:35 +0500

    export type ObservableMapObject

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
-export {resizeObserver, pauseObserving, resumeObserving} from "./resize_sensor";
-export {Engine} from "./engine";
-export {Layout} from "./layout";
+export {
+    resizeObserver,
+    pauseObserving,
+    resumeObserving,
+} from './resize_sensor';
+export { Engine } from './engine';
+export { Layout } from './layout';
 import './vlayout.css';
 
 export { ReactViewReference as ReactView } from './react_views';
-export type { ReactViewProps, ReactViewState } from "./react_views";
-export {View} from "./view";
+export type { ReactViewProps, ReactViewState } from './react_views';
+export { View } from './view';
 export { ColorContainer as Color, ImageContainer as Image } from './types';
-export type { SimpleListDefinitionItem, ListDefinitionItem, ComplexDefinitionItem } from './types';
+export type {
+    SimpleListDefinitionItem,
+    ListDefinitionItem,
+    ComplexDefinitionItem,
+} from './types';
 export type { ListModelItem } from './list';
-export { LayoutComponent } from './layout_component'
-export { connect } from './connect'
+export { LayoutComponent } from './layout_component';
+export { connect } from './connect';
+export type { ObservableMapObject } from './connect';
