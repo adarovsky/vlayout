1ced9d8ee8dace64b60dd7fbdbace39a78ed9c1e
Alexander Darovsky <adarovsky@me.com>
2022-04-12 14:37:34 +0500

    depend on react 17

--- test/setupEnzyme.ts
+++ test/setupEnzyme.ts
@@ -1,5 +1,5 @@
 import 'react-app-polyfill/stable';
 import {configure} from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new EnzymeAdapter() });
